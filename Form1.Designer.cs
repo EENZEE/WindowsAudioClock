fa1a6bed53a81e8dab64616fc3311a209dd3ec00
Albert Straub <Albert Straub>
2017-04-27 12:42:18 -0600

    Audio Clock Source Code Windows 10 compatible.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class AudioClockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.hourCheckBox = new System.Windows.Forms.CheckBox();
+            this.quarterHourCheckBox = new System.Windows.Forms.CheckBox();
+            this.halfHourCheckBox = new System.Windows.Forms.CheckBox();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.allMinutesCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(28, 230);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(75, 23);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // hourCheckBox
+            // 
+            this.hourCheckBox.AutoSize = true;
+            this.hourCheckBox.Location = new System.Drawing.Point(28, 23);
+            this.hourCheckBox.Name = "hourCheckBox";
+            this.hourCheckBox.Size = new System.Drawing.Size(49, 17);
+            this.hourCheckBox.TabIndex = 1;
+            this.hourCheckBox.Text = "Hour";
+            this.hourCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // quarterHourCheckBox
+            // 
+            this.quarterHourCheckBox.AutoSize = true;
+            this.quarterHourCheckBox.Location = new System.Drawing.Point(28, 47);
+            this.quarterHourCheckBox.Name = "quarterHourCheckBox";
+            this.quarterHourCheckBox.Size = new System.Drawing.Size(87, 17);
+            this.quarterHourCheckBox.TabIndex = 2;
+            this.quarterHourCheckBox.Text = "Quarter Hour";
+            this.quarterHourCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // halfHourCheckBox
+            // 
+            this.halfHourCheckBox.AutoSize = true;
+            this.halfHourCheckBox.Location = new System.Drawing.Point(28, 71);
+            this.halfHourCheckBox.Name = "halfHourCheckBox";
+            this.halfHourCheckBox.Size = new System.Drawing.Size(71, 17);
+            this.halfHourCheckBox.TabIndex = 3;
+            this.halfHourCheckBox.Text = "Half Hour";
+            this.halfHourCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(53, 143);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(174, 46);
+            this.timeLabel.TabIndex = 4;
+            this.timeLabel.Text = "00:00:00";
+            // 
+            // allMinutesCheckBox
+            // 
+            this.allMinutesCheckBox.AutoSize = true;
+            this.allMinutesCheckBox.Location = new System.Drawing.Point(28, 95);
+            this.allMinutesCheckBox.Name = "allMinutesCheckBox";
+            this.allMinutesCheckBox.Size = new System.Drawing.Size(77, 17);
+            this.allMinutesCheckBox.TabIndex = 5;
+            this.allMinutesCheckBox.Text = "All Minutes";
+            this.allMinutesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // AudioClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.allMinutesCheckBox);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.halfHourCheckBox);
+            this.Controls.Add(this.quarterHourCheckBox);
+            this.Controls.Add(this.hourCheckBox);
+            this.Controls.Add(this.PlayButton);
+            this.Name = "AudioClockForm";
+            this.Text = "Audio Clock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.CheckBox hourCheckBox;
+        private System.Windows.Forms.CheckBox quarterHourCheckBox;
+        private System.Windows.Forms.CheckBox halfHourCheckBox;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.CheckBox allMinutesCheckBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+
+
+namespace WindowsFormsApplication1
+{
+    public delegate void UpdateDisplayCallBack(string text);
+
+    public partial class AudioClockForm : Form
+    {
+        System.Media.SoundPlayer player =
+           new System.Media.SoundPlayer();
+        public static System.Timers.Timer myTimer = new System.Timers.Timer();
+        private string lastString;
+ 
+        public AudioClockForm()
+        {
+            InitializeComponent();
+            myTimer.Elapsed += new ElapsedEventHandler( DisplayTimeEvent );
+            myTimer.Interval = 1000;
+            myTimer.Start();
+
+        }
+
+        public void DisplayTimeEvent( object source, ElapsedEventArgs e )
+        {
+            int hours = 0;
+            int allMinutes = 0;
+            int singleMinutes = 0;
+
+            string nowString = DateTime.Now.ToShortTimeString();
+
+            // nowString = "9:20 PM";
+            // lastString = "9:19 PM";
+
+            if (nowString != lastString)
+            {
+                UpdateDisplay(nowString);
+                lastString = nowString;
+                char[] tags = {':', ' '};
+                string[] timeArray = nowString.Split(tags);
+
+                if (int.TryParse(timeArray[1], out allMinutes))
+                {
+
+                }
+
+                if (int.TryParse(timeArray[0], out hours))
+                {
+                    singleMinutes = allMinutes % 10;
+
+                    if (allMinutes == 0 && hourCheckBox.Checked)
+                    {
+                        saySomething(timeArray[0]);
+                        saySomething("OCLOCK");
+                    }
+                    else if ((quarterHourCheckBox.Checked && 
+                             (allMinutes == 15 || allMinutes == 30 || allMinutes == 45 || allMinutes == 0) 
+                             || (halfHourCheckBox.Checked && (allMinutes == 30)) 
+                             || (hourCheckBox.Checked && allMinutes == 0) 
+                             || allMinutesCheckBox.Checked))  
+                    {
+                        saySomething(timeArray[0]);
+
+                        if (allMinutes < 20)
+                        {
+                            // Do nothing.
+                        }
+                        else if (allMinutes < 30)
+                        {
+                            saySomething("20");
+                        }
+                        else if (allMinutes < 40)
+                        {
+                            saySomething("30");
+                        }
+                        else if (allMinutes < 50)
+                        {
+                            saySomething("40");
+                        }
+                        else
+                        {
+                            saySomething("50");
+                        }
+
+                        if (singleMinutes != 0 && allMinutes < 10)
+                        {
+                            saySomething("0");
+                            saySomething(Convert.ToString(allMinutes));
+                        }
+                        else if (allMinutes != 0 && allMinutes < 20)
+                        {
+                            saySomething(Convert.ToString(allMinutes));
+                        }
+                        else if (singleMinutes!=0)
+                        {
+                            saySomething(Convert.ToString(singleMinutes));
+                        }
+
+                        saySomething(timeArray[2]);
+                    }
+                }
+            }
+        }
+
+
+        private void UpdateDisplay(string text)
+        {
+
+            if (timeLabel.InvokeRequired)
+            {
+                UpdateDisplayCallBack d = new UpdateDisplayCallBack(this.UpdateDisplay);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.timeLabel.Text = text;
+            }
+
+            
+        }
+
+
+        private void saySomething(string something)
+        {
+            string file = null;
+
+            switch (something)
+            {
+                case "AM":
+                    file = ".\\resources\\AM.wav";
+                    break;
+                case "PM":
+                   file = ".\\resources\\PM.wav";
+                    break;
+                case "00":
+                case "OCLOCK":
+                   file = ".\\resources\\OCLOCK.wav";
+                    break;
+                case "0":
+                    file = ".\\resources\\OH.wav";  // fix this
+                    break;
+                case "1":
+                    file = ".\\resources\\ONE.wav"; 
+                    break;
+
+                case "2":
+                    file = ".\\resources\\TWO.wav"; 
+                    break;
+                case "3":
+                    file = ".\\resources\\THREE.wav"; 
+                    break;
+                case "4":
+                    file = ".\\resources\\FOUR.wav"; 
+                    break;
+                case "5":
+                    file = ".\\resources\\FIVE.wav"; 
+                    break;
+                case "6":
+                    file = ".\\resources\\SIX.wav"; 
+                    break;
+                case "7":
+                    file = ".\\resources\\SEVEN.wav"; 
+                    break;
+                case "8":
+                    file = ".\\resources\\EIGHT.wav"; 
+                    break;
+                case "9":
+                    file = ".\\resources\\NINE.wav"; 
+                    break;
+                case "10":
+                    file = ".\\resources\\TEN.wav"; 
+                    break;
+                case "11":
+                    file = ".\\resources\\ELEVEN.wav"; 
+                    break;
+                case "12":
+                    file = ".\\resources\\TWELVE.wav"; 
+                    break;
+                case "13":
+                    file = ".\\resources\\THIRTEEN.wav";
+                    break;
+                case "14":
+                    file = ".\\resources\\FOURTEEN.wav"; 
+                    break;
+                case "15":
+                    file = ".\\resources\\FIFTEEN.wav"; 
+                    break;
+                case "16":
+                    file = ".\\resources\\SIXTEEN.wav"; 
+                    break;
+                case "17":
+                    file = ".\\resources\\SEVENTEEN.wav"; 
+                    break;
+                case "18":
+                    file = ".\\resources\\EIGHTEEN.wav"; 
+                    break;
+                case "19":
+                    file = ".\\resources\\NINETEEN.wav"; 
+                    break;
+                case "20":
+                    file = ".\\resources\\TWENTY.wav"; 
+                    break;
+                case "30":
+                    file = ".\\resources\\THIRTY.wav";
+                    break;
+                case "40":
+                    file = ".\\resources\\FOURTY.wav";
+                    break;
+                case "50":
+                    file = ".\\resources\\FIFTY.wav";
+                    break;
+                case "60":
+                    file = ".\\resources\\SIXTY.wav";
+                    break;
+                case "70":
+                    file = ".\\resources\\SEVENTY.wav";
+                    break;
+            }
+
+            if (file != null)
+            {
+                playSound(file);
+            }
+        }
+
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            playSound(".\\resources\\ONE.wav");
+            playSound(".\\resources\\TWENTY.wav");
+            playSound(".\\resources\\TWO.wav");
+            saySomething("OCLOCK");
+        }
+
+        private void playSound(string path)
+        {
+
+            player.SoundLocation = path;
+            player.Load();
+            player.PlaySync();
+            player.Dispose();
+        }
+    }
+}
